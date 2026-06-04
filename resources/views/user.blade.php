33c75d5405df4bdecdfb0dc208230c1406f3bf60
junaid <mohmmadjunaidqurashi@gmail.com>
2026-06-04 12:21:31 +0530

    bootstrap table se sara data dikhaya

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,4 +1,40 @@
-<h1>All Users List</h1>
-@foreach ($data as $id => $user)
-    <h3>{{ $user->name }} |{{ $user->email }} |{{ $user->city }} </h3>
-@endforeach
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>All User</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-4">
+        <div class="row   justify-content-center">
+            <div class="col-6">
+                <table class="table table-bordered table-striped">
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>City</th>
+                    </tr>
+
+                    @foreach ($data as $id => $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->city }}</td>
+                        </tr>
+                    @endforeach
+
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
