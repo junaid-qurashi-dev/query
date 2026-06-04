025c5600d792f15c195471598931149e5c81f9cc
junaid <mohmmadjunaidqurashi@gmail.com>
2026-06-04 12:22:58 +0530

    kisi specific record ko nikalne kai liye where ka use

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function ShowUser()
     {
-        $users = DB::table('users')->get();
+        $users = DB::table('users')->where('id', 3)->get();
 
         // Return Data In josn formate //
         return view('user', ['data' => $users]);
